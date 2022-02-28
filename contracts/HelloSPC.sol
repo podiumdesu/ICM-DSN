e9292f8fe21e29f371f65a9301c31ae6898c5c21
podiumdesu <wwhmomo@gmail.com>
2022-02-28 14:41:45 +0800

    Add: comments

--- contracts/HelloSPC.sol
+++ contracts/HelloSPC.sol
@@ -19,58 +19,61 @@ contract Test is ChainlinkClient {
     string public external_address;
 
     struct TaskChallenge {
-        uint sliceIndex;
-        // add timestamp
-        bool result;
+        uint sliceIndex;      // which slice no chosen to be challenged
+        // TODO: add timestamp for signature
+        bool result;            // challenge result
     }
 
 
     struct Task {
-        uint time;
-        string root;
-        uint price;
-        address owner;
-        address sp;
+        uint time;    // expected storage time
+        string root;    // merkle root value
+        uint price;     // money to be paid 
+        address owner;    // owner address
+        address sp;       // storage provider address
         // TaskChallenge[] challengeList;
     }
 
+    // for signature, useless now
     mapping(address => string) public pkList;
 
+    // get challenge result
     mapping(address => bool) public challengeResult;
 
+    // map request ID with customer address (in order to put result back)
     mapping (bytes32 => address) public requestMapping;
 
+    // map sp address to its api, which will be transferred to external adapter to request
+    mapping(address => string) private externalApiList;
 
+    // map user address to his tasks
+    mapping(address => Task[]) public userTasksMapping;
 
+    // CHECK: user needs to create storage tasks before 
     modifier sysUser {
         require(userTasksMapping[msg.sender].length > 0, "It seems that you do not use our system ><");
         _;
     }
 
-    mapping(address => string) private externalApiList;
     constructor() {
-        // set default oracle
+        // set default oracle external adapter
         externalApiList[0x7C152F84A11A4f64aB69694D41c0b68414Aec7E5] = "20.212.155.179/challenge/";
 
-
+        // set default oracle address and jobID
         setPublicChainlinkToken();
         oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
         // jobId = "d5270d1c311941d0b08bead21fea7747";    uint256
-        jobId = "bc746611ebee40a3989bbe49e12a02b9";
+        jobId = "bc746611ebee40a3989bbe49e12a02b9";   // bool
         fee = 0.1 * 10 ** 18; // (Varies by network and job)
     }
-    mapping(address => Task[]) public userTasksMapping;
 
-    // Challenge Work
-    // (string memory targetRoot, uint index)
-    // returns (bytes32 requestId) 
-   function requestVolumeData(string memory merkle, string memory slice) public sysUser
+    // send oracle request
+   function requestChallengeResult(string memory merkle, string memory slice) public sysUser
     {
         Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
         
         // Set the URL to perform the GET request on
         external_address = string(abi.encodePacked("http://20.212.155.179:3000/oracle/", externalApiList[0x7C152F84A11A4f64aB69694D41c0b68414Aec7E5] , merkle, "/", slice));
-        // request.add("get", "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD");
         request.add("get", external_address);
 
         // Set the path to find the desired data in the API response, where the response format is:
@@ -83,7 +86,7 @@ contract Test is ChainlinkClient {
     }
     
     /**
-     * Receive the response in the form of uint256
+     * Receive the response in the form of bool
      */ 
     function fulfill(bytes32 _requestId, bool _volume) public recordChainlinkFulfillment(_requestId)
     {
@@ -92,22 +95,13 @@ contract Test is ChainlinkClient {
         volume = _volume;
     }
 
-
-
-
-
-    // Set the public key
+    // Set the public key, useless now
     function setPk(string memory pk) public {
         pkList[msg.sender] = pk;
     }
 
-    function setBook(uint time, string memory root, uint price) public {
-        // Task memory task = Task(time, root, price, msg.sender, address(0x0), 100);
-        //   address[] memory minters = new address[](1);
-        // minters[0] = minter;
-        // StandaloneERC20.initialize("MyToken", "MTK", uint8(18), minters, pausers);
-        // TaskChallenge[] storage tempList;
-        // tempList.push({sliceIndex: 0, result: false});
+    // set storage task
+    function setTask(uint time, string memory root, uint price) public {
         Task memory task = Task(time, root, price, msg.sender, address(0x7C152F84A11A4f64aB69694D41c0b68414Aec7E5));
         userTasksMapping[msg.sender].push(task);
     }
